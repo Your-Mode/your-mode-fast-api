033f235f45b768a83ae5b9988c6ca128dbddb6bd
Tnalxmsk <alstnwkd990@naver.com>
2025-06-24 23:58:37 +0900

    :bug: fix: 404 Error 수정

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-# app/main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.assistant import router as assistant_router
+from mangum import Mangum
+import logging
 
 app = FastAPI()
+logger = logging.getLogger("uvicorn.access")
+
+@app.middleware("http")
+async def log_path(request: Request, call_next):
+    logger.info(f"▶▶ Raw request path: {request.url.path}")
+    return await call_next(request)
+
 app.include_router(assistant_router, prefix="/assistant")
+handler = Mangum(app, api_gateway_base_path="/prod")
