c05f6c9e6deee548f1acf516c009243d41645727
Tnalxmsk <alstnwkd990@naver.com>
2025-06-24 11:18:46 +0900

    :wrench: chore: python version update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
+# app/main.py
 from fastapi import FastAPI
-from mangum import Mangum
-
 from app.api.assistant import router as assistant_router
+
 app = FastAPI()
 app.include_router(assistant_router, prefix="/assistant")
-
-handler = Mangum(app)
